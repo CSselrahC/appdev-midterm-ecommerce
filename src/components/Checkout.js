19831bfda3055779be252df8a9394347d7a8bb57
kurtysixCagayan <truknohjnayagac@gmail.com>
2025-10-19 17:53:54 +1200

    edited checkout

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,1 +1,22 @@
-// checkout
+import React, {useState} from 'react';
+
+function Checkout() {
+    const [cartList, setCartList] = useState([]);
+
+    return (
+        <div>
+            <h1>Shopping Cart</h1>
+            <p>Your selected products:</p>
+            <ul>
+                {cartList.map(item => (
+                    <li key={item.id}>
+                        {item.name} - ₱{item.price.toFixed(2)}
+                        <button>Remove from Cart</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Checkout;
